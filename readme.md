48bed49edb9abce7a53910974b1a213b399dfbab
Patrick Stein <lylepstein@gmail.com>
2013-08-02 14:37:30 -0700

    Import code, document, and add bower config

---
+++ readme.md
@@ -0,0 +1,116 @@
+# vz.touch
+
+A cross-browser unified click/touch handling library from
+[Vizify](https://www.vizify.com).  It's small, it's fast, it solves
+all your problems.
+
+It has no dependencies and works right against the DOM API.  Older
+versions of IE are not supported.
+
+## Example
+
+```javascript
+vz.touch($('#mything'), {
+  click: function(e) {
+    alert('Clicked!')
+  }
+});
+
+## What it does
+
+There are a few buckets of functionality `vz.touch` provides:
+
+ * Works around browser bugs
+ * Provides a normalized interface across touch and click events
+ * Provides useful extra information with its events (e.g. cursor position)
+
+With the exception of the Android 4.x stock browser, `vz.touch` supports
+touch and click events *in tandem*, so if you've got, say, a touchscreen
+laptop with a mouse plugged in, you can use either with no problems.
+
+One of the main benefits you'll get from `vz.touch` is that it handles
+any combination of events easily, which is a bit of a feat if you're
+trying to do it yourself.  For example, you can bind `click` and `drag`
+and both will be fired just as you expect.  It's non-trivial to accomplish.
+
+The short version of all this: `vz.touch` does nothing magical.  It just
+slogs through the muck so you get the best available touch/click support
+with a nice API, without having to work for it.  It's the touch library
+you'd write if you had the time.
+
+## How events are fired
+
+**click**
+Native `click` event, or `touchstart` is followed by a `touchend` with
+less than `DRAG_THRESHOLD` (3px) amount of movement in between.
+
+**down**
+Native `mousedown` or `touchstart` events.
+
+**up**
+Native `mouseup` or `touchend` events.
+
+**drag**
+No native equivalent; if a `down` is followed by enough mouse movement
+drag mode will be entered until an `up`.  Important: the drag will
+continue even if it goes out of the bounds of the original element
+the drag started on.  This is the behavior you want, because it prevents
+UI janks from prematurely ending the drag.
+
+## API
+
+Binding is pretty easy:
+
+```javascript
+var killer = vz.touch(el, options, events);
+```
+
+ * `el` - Either a DOM element or an Array-like list of DOM elements (e.g.
+          jQuery, `Array`, `NodeList`) to bind events to.
+ * `options` - **optional** Options map.  Currently there are no options...
+ * `events` - Map of events to fire.  See above section for supported
+              event names
+
+The return value is a killer function that should be called once to
+unbind the events.
+
+**Note:** Calling `vz.touch` more than once on the same element will
+          result in weird behavior.  Don't do it!
+
+Event handlers are supplied a single event argument, just like a native
+handler.  Similarly, `this` inside a handler will be set to the element
+that triggered the event.  Note that, unlike a native event handler,
+`preventDefault()` has already been called.  A dummy one exists on the
+event argument for backwards compatibility, but it does nothing.
+
+### The event object
+
+This guy's a little custom.  Here's the spec:
+
+```javascript
+{
+  preventDefault: Function, // Does nothing
+  stopPropagation: Function, // Native stopPropagation functionality
+  target: Element, // Source of the event,
+  absolute: {
+    t: Number, // Unix timestamp the event occurred at
+    x: Number, // Window X coordinate of the event
+    y: Number // Window Y cooridnate of the event
+  },
+
+  // These properties appear on drag events only
+  relative: {
+    t: Number, // Milliseconds elapsed since the start of the drag
+    x: Number, // Pixels dragged along the X axis during the drag
+    y: Number // Pixels dragged along the Y axis during the drag
+  },
+  delta: {
+    t: Number, // Milliseconds since the last drag event
+    x: Number, // Pixels dragged along the X axis since the last drag event
+    y: Number // Pixels dragged along the Y axis since the last drag event
+  },
+  dragState: Number // 0 for starting (fired on first drag event)
+                    // 1 for continuing
+                    // 2 for ending (fired on last drag event)
+}
+```
---
+++ vztouch.js
@@ -0,0 +1,398 @@
+// TODO - allowDragScroll: 'horizontal' || 'vertical'
+
+//
+// Quick usage:
+//
+//     vz.touch(el, [options], events);
+//
+//  * el - A DOM element, or array-like container of DOM elements (e.g.
+//         jQuery, Array, NodeList).
+//
+//  * options - Optional map of options to use.  None currently
+//              implemented.
+//
+//  * events - Map of events to bind.  Supported events: click down up
+//             drag.
+//
+// Event handler work largely like regular old DOM event handlers.  The
+// `this` object is set to the target of the event, and a single event
+// argument is passed through defined below.  Note that `preventDefault`
+// is always called before the event is passed to the handler, but a
+// dummy `preventDefault` method is still provided for compatibility.
+//
+// Event interface:
+// {
+//   stopPropagation: function,
+//   preventDefault: dummy function,
+//   target: target element,
+//   absolute/relative/delta: {
+//     t: time
+//     x: window x position
+//     y: window y position
+//   }
+//   dragState: 0|1|2 // starting, continuing, ended
+// }
+//
+// Note that relative, delta, and dragState ONLY appear for drag events
+
+(function() {
+
+  ///////////////////////////////////////////////
+  // Internal flags
+  ///////////////////////////////////////////////
+
+  // When true, click events will be fully emulated and the native
+  // click event never bound.
+  var ignoreNativeClick = false;
+
+  // Sets how many pixels along an axis must be moved to consider an
+  // action a drag.
+  var DRAG_THRESHOLD = 3;
+
+  // If we're using the stock android browser, we *cannot* bind to
+  // click events because the touch events cannot be properly cancelled.
+  if (navigator.userAgent.match(/Android/) && !navigator.userAgent.match(/Firefox|Chrome/))
+    ignoreNativeClick = true;
+
+  // Does extra logging when set
+  var DEBUG = false;
+
+  /////////////////////////////////////////////
+  // Public Interface
+  /////////////////////////////////////////////
+
+  // Ensure namespace exists
+  if (!window.vz)
+    window.vz = {};
+
+  // Event types: down, up, drag, click
+  vz.touch = function(el, opts, events) {
+    // Sanity
+    if (!el)
+      return function() {};
+
+    // Handle jQuery, arrays, etc
+    if (el.length) {
+      var cleanups = [];
+      for (var i=0; i<el.length; i++)
+        cleanups.push(vz.touch(el[i], opts, events));
+      return function() {
+        for (var i=0; i<cleanups.length; i++)
+          cleanups[i]();
+      };
+    }
+
+    // Opts is optional
+    if (!events) {
+      events = opts;
+      opts = undefined;
+    }
+    if (!opts)
+      opts = {};
+
+    // Validate events and options
+    for (var i in events) if (events.hasOwnProperty(i))
+      if (['down', 'up', 'drag', 'click'].indexOf(i) < 0)
+        throw new Error('Unsupported event "' + i + '"');
+    for (var i in opts) if (opts.hasOwnProperty(i))
+      if (['allowDragScroll'].indexOf(i) < 0)
+        throw new Error('Unsupported option "' + i + '"');
+
+    ////////////////////////////////////////////////////
+    // Implementation
+    ////////////////////////////////////////////////////
+
+    // Drag state
+    var dragInfo = {
+      // Start pos
+      sx: 0,
+      sy: 0,
+      // Current pos
+      x: 0,
+      y: 0,
+      // Total movement
+      tx: 0,
+      ty: 0,
+
+      // Start time
+      sd: 0,
+      // Current time
+      d: 0,
+      // Total time
+      td: 0,
+
+      // Target drag element
+      target: null,
+
+      // Current drag state
+      state: 0,
+    };
+
+    // Gets controlled by drag and touch events.  When drag goes over
+    // a threshold the next click is ignored.  When a touchend finishes
+    // WITHIN threshold it will fire click instantly and set this so
+    // that the regular touch event doesn't occur.
+    var ignoreThisClick = false;
+
+    // When true, drag events are bound to the window.  Reset after
+    // a drag ends.
+    var dragEventsBound = true;
+
+    // When true, we have sent a drag event to the user and will need
+    // to follow up by sending a final null drag to signal the end.
+    var dragFired = false;
+
+    // Special handler for preventing text selection while dragging
+    var selectionKiller = function(e) {
+      if (DEBUG)
+        console.log('selectionKiller');
+
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    };
+
+    // Special handler for ending drags regardless of where the cursor is
+    var dragEnder = function(e) {
+      if (DEBUG)
+        console.log('dragEnder', e ? e.type : undefined);
+
+      window.removeEventListener('mousemove', drag, false);
+      window.removeEventListener('touchmove', drag, false);
+      window.removeEventListener('mouseup', dragEnder, true);
+      window.removeEventListener('touchend', dragEnder, true);
+      window.removeEventListener('selectstart', selectionKiller, true);
+
+      if (dragEventsBound) {
+        dragEventsBound = false;
+        if (events.drag && dragFired) {
+          dragFired = false;
+          events.drag.call(dragInfo.target, {
+            stopPropagation: function() { e && e.stopPropagation() },
+            preventDefault: function() {},
+            target: dragInfo.target,
+            absolute: {x: dragInfo.x, y: dragInfo.y, t: +new Date()},
+            relative: {x: dragInfo.x - dragInfo.sx,
+                       y: dragInfo.y - dragInfo.sy,
+                       t: (+new Date()) - dragInfo.st
+                      },
+            delta: {x: 0, y: 0, t: (+new Date()) - dragInfo.t},
+            dragState: 2
+          });
+        }
+      }
+    };
+
+    var down = function(e) {
+      if (DEBUG)
+        console.log('down', e.type);
+
+      // Ignore multitouches or non-primary mousebutton clicks
+      if ((e.touches && e.touches.length > 2) || (e.button !== undefined && e.button !== 0))
+        return;
+
+      // This is the start of a new click; don't ignore it
+      ignoreThisClick = false;
+
+      // If the user's listening for down events, pass on through
+      if (events.down) {
+        e.preventDefault();
+        events.down.call(this, {
+          stopPropagation: function() { e.stopPropagation(); },
+          preventDefault: function() {},
+          target: e.target,
+          absolute: {
+            x: e.touches && e.touches.length ? e.touches[0].pageX : e.clientX,
+            y: e.touches && e.touches.length ? e.touches[0].pageY : e.clientY,
+            t: +new Date()
+          }
+        });
+      }
+
+
+      // For dragging via mouse-type events, disable text selection.
+      if (events.drag || e.type == 'mousedown') {
+        e.preventDefault();
+      }
+
+      // For drag events bootstrap the position and element
+      if (events.drag || e.type == 'touchstart') {
+
+        // Set drag starting coordinates
+        if (e.touches) {
+          dragInfo.sx = e.touches[0].pageX;
+          dragInfo.sy = e.touches[0].pageY;
+        } else {
+          dragInfo.sx = e.clientX;
+          dragInfo.sy = e.clientY;
+        }
+        dragInfo.x = dragInfo.sx;
+        dragInfo.y = dragInfo.sy;
+        dragInfo.tx = 0;
+        dragInfo.ty = 0;
+        dragInfo.t = dragInfo.st = +new Date();
+        dragInfo.target = e.target;
+        dragInfo.state = 0;
+
+        // Bind to drag events
+        window.addEventListener('mousemove', drag, false);
+        window.addEventListener('touchmove', drag, false);
+        window.addEventListener('mouseup', dragEnder, true);
+        window.addEventListener('touchend', dragEnder, true);
+        window.addEventListener('selectstart', selectionKiller, true);
+        dragEventsBound = true;
+      }
+    };
+
+    var up = function(e) {
+      if (DEBUG)
+        console.log('up', e.type);
+
+      // If the user's listening for up events, pass on through
+      if (events.up) {
+        e.preventDefault();
+        events.up.call(this, {
+          stopPropagation: function() { e.stopPropagation(); },
+          preventDefault: function() {},
+          target: e.target,
+          absolute: {
+            x: e.touches && e.touches.length ? e.touches[0].pageX : e.clientX,
+            y: e.touches && e.touches.length ? e.touches[0].pageY : e.clientY,
+            t: +new Date()
+          }
+        });
+      }
+
+      // Optimized click delivery
+      if (events.click && !ignoreThisClick) {
+        // If we're ignoring native click, simulate a click event.
+        // Similarly, if this event came from a touchend, we can trigger
+        // our own click and ignore the real click event, which will
+        // give snappier performance.
+        if (ignoreNativeClick || e.type == 'touchend') {
+          e.preventDefault();
+          ignoreThisClick = true;
+          events.click.call(this, {
+            stopPropagation: function() { e.stopPropagation(); },
+            preventDefault: function() {},
+            target: e.target,
+            absolute: {
+              x: e.touches && e.touches.length ? e.touches[0].pageX : e.clientX,
+              y: e.touches && e.touches.length ? e.touches[0].pageY : e.clientY,
+              t: +new Date()
+            }
+          });
+        }
+      }
+    };
+
+    var click = function(e) {
+      if (DEBUG)
+        console.log('click', e.type);
+
+      if (!ignoreThisClick && events.click) {
+        events.click.call(this, {
+          stopPropagation: function() { e.stopPropagation() },
+          preventDefault: function() {},
+          target: e.target,
+          absolute: {
+            x: e.touches ? e.touches[0].clientX : e.clientX,
+            y: e.touches ? e.touches[0].clientY : e.clientY,
+            t: +new Date()
+          }
+        });
+      }
+    };
+
+    var drag = function(e) {
+      if (DEBUG)
+        console.log('drag', e.type);
+
+      // Don't handle multitouch
+      if (e.touches && e.touches.length > 1)
+        return;
+
+      // New position
+      var nx = 0;
+      var ny = 0;
+      if (e.touches) {
+        nx = e.touches[0].pageX;
+        ny = e.touches[0].pageY;
+      } else {
+        nx = e.clientX;
+        ny = e.clientY;
+      }
+
+      // New date
+      var nt = +new Date();
+
+      // Prep the data to pass to the client
+      var data = {
+        stopPropagation: function() { e.stopPropagation() },
+        preventDefault: function() {},
+        target: dragInfo.target,
+        absolute: {x: nx, y: ny, t: nt},
+        relative: {x: nx - dragInfo.sx, y: ny - dragInfo.sy, t: nt - dragInfo.st},
+        delta: {x: nx - dragInfo.x, y: ny - dragInfo.y, t: nt - dragInfo.t},
+        dragState: dragInfo.state
+      };
+
+      // Update the last position
+      dragInfo.t = nt;
+      dragInfo.x = nx;
+      dragInfo.y = ny;
+      dragInfo.tx += Math.abs(data.delta.x);
+      dragInfo.ty += Math.abs(data.delta.y);
+
+      // When true, we've passed the drag threshold and are treating
+      // this drag as a drag and not a sloppy click.
+      var overThreshold = dragInfo.tx > DRAG_THRESHOLD || dragInfo.ty > DRAG_THRESHOLD;
+
+      // If we've moved more than the drag threshold on either axis, stop
+      // click from happening.
+      if (!ignoreThisClick && overThreshold)
+        ignoreThisClick = true;
+
+      // If we've moved more than the drag treshold on the configured
+      // axis (both by default), prevent native scroll from working and
+      // then drigger the drag business.
+      if (events.drag && overThreshold) {
+        e.preventDefault();
+
+        // Pass the drag event on down
+        if (events.drag) {
+          events.drag.call(dragInfo.target, data);
+          dragFired = true;
+          dragInfo.state = 1;
+        }
+
+        // TODO - Support allowances for scrolling in one direction
+      }
+    };
+
+    // Bind all our events
+    el.addEventListener('touchstart', down, false);
+    el.addEventListener('touchend', up, false);
+    if (!ignoreNativeClick) {
+      el.addEventListener('mousedown', down, false);
+      el.addEventListener('mouseup', up, false);
+      el.addEventListener('click', click, false);
+    }
+
+    // Return a cleanup function
+    return function() {
+      el.removeEventListener('touchstart', down);
+      el.removeEventListener('touchend', up);
+      if (!ignoreNativeClick) {
+        el.removeEventListener('mousedown', down);
+        el.removeEventListener('mouseup', up);
+        el.removeEventListener('click', click);
+      }
+
+      if (dragEventsBound) {
+        dragEventsBound = false;
+        dragEnder();
+      }
+    };
+  };
+})();
